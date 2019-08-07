eb3f1a2aaf372bf365cf353a21abf37cbba6003d
pedeveloper2019 <pedeveloper.2019@gmail.com>
2019-08-07 17:20:14 +0530

    Contact-us changes done

--- assets/js/header.js
+++ assets/js/header.js
@@ -6,7 +6,17 @@ $(function () {
     $("#top-navigation .nav-item").mouseleave(function () { 
         $("#top-navigation .nav-item.active").css("background","#1c7bd6")
     });
-     $(".navbar").css({"background-image":"url('assets/images/top-nav-bg.png')","background-repeat":"no-repeat","background-size":"cover"})
-     $("#top-navigation").css({"background":"none"})
+
+    $(".navbar").css({"background-image":"url('assets/images/top-nav-bg.png')","background-repeat":"no-repeat","background-size":"cover"})
+    $("#top-navigation").css({"background":"none"})
+
+    $("#contact-us-cards .card").mouseenter(function () { 
+        $(this).css({"transition" : "all .5s ease-out",
+            "transform": "scale(1.1)"});
+    });
+    $("#contact-us-cards .card").mouseleave(function () { 
+        $("#contact-us-cards .card").css({"transition" : "all .5s ease-out",
+            "transform": "scale(1)"});
+    });
      
 });
